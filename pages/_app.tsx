2aa217de366aaeea9153e8a272207d98f40e9b91
B-Mustansir <mustansirzain2@gmail.com>
2024-10-20 20:37:51 +0530

    update chain to base for swap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,20 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import { defineChain } from "thirdweb";
+import { useEffect } from "react";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "mumbai";
+const customChain = defineChain({chainId: 84532});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={process.env.THIRDWEB_CLIENT_ID}
-      activeChain={activeChain}
+      supportedChains={[customChain]}
+      activeChain={customChain}
     >
       <Navbar />
       <Component {...pageProps} />
